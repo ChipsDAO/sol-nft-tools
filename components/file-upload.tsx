c30833200a9681674862699075c108e96cf00fbb
Alice <alize_in_cryptoland@protonmail.com>
2021-10-11 18:58:13 +0200

    Add arweave uploader

---
+++ components/file-upload.tsx
@@ -0,0 +1,42 @@
+import { Upload, message } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
+
+const { Dragger } = Upload;
+
+export function FileUpload({setFiles}) {
+
+const props = {
+  name: "file",
+  multiple: true,
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (status === "done") {
+      setFiles(info.fileList.map(f => f.originFileObj));
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log("Dropped files", e.dataTransfer.files);
+  },
+};
+
+  return (
+    <Dragger {...props}>
+      <p className="ant-upload-drag-icon">
+        <InboxOutlined />
+      </p>
+      <p className="ant-upload-text">
+        Click or drag file to this area to upload
+      </p>
+      <p className="ant-upload-hint">
+        Support for a single or bulk upload. Strictly prohibit from uploading
+        company data or other band files
+      </p>
+    </Dragger>
+  );
+}
