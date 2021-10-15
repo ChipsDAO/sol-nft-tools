78f7283d8b37a3510e9fbdc3dc60d641fceea3a1
Alice <alize_in_cryptoland@protonmail.com>
2021-10-15 19:00:44 +0200

    Update endpoint

--- util/endpoints.ts
+++ util/endpoints.ts
@@ -3,7 +3,7 @@ import { clusterApiUrl } from "@solana/web3.js";
 export const ENDPOINTS = [
   {
     name: "GenesysGo",
-    endpoint: "https://gate2.main.genesysgo.net:8899",
+    endpoint: "https://lokidfxnwlabdq.main.genesysgo.net:8899",
   },
   {
     name: "Metaplex",
