9be74687f8c1195d15be2fca049cdf830bc60361
Alice <alize_in_cryptoland@protonmail.com>
2021-10-27 16:04:08 +0200

    branding

--- components/meta.tsx
+++ components/meta.tsx
@@ -4,32 +4,32 @@ import Head from "next/head";
 export const Meta = () => {
   return (
     <Head>
-          <title>Solana NFT Toolbox</title>
-          <meta name="description" content="grab metadata from SOL NFTs" />
-          <link rel="icon" href="/favicon.ico" />
+          <title>Solana NFT Tools</title>
+          <meta name="description" content="Solana NFT Tools" />
+          <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⭐</text></svg>"/>
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@0xAlice_" />
-          <meta name="twitter:creator" content="@0xAlice_" />
-          <meta property="og:site_name" content="Solana NFT Toolbox" />
+          <meta name="twitter:site" content="@pentaclexyz" />
+          <meta name="twitter:creator" content="@pentaclexyz" />
+          <meta property="og:site_name" content="Solana NFT Tools" />
           <meta
             name="twitter:image"
             content="https://arweave.net/jeC5bLjY2mY7mZJvnJ1aHTP1zh3oUSMGsalcrUwwJ28"
           />
           <meta
             name="twitter:image:alt"
-            content="Solana NFT Tools, made by @0xAlice_"
+            content="Solana NFT Tools, made by @pentaclexyz"
           />
-          <meta name="twitter:title" content="Solana NFT Toolbox" />
+          <meta name="twitter:title" content="Pentacle SOL NFT Tools" />
           <meta
             name="twitter:description"
-            content="Solana NFT Tools, made by @0xAlice_"
+            content="Solana NFT Tools, made by @pentaclexyz"
           />
           <meta name="og:url" content="https://sol-nft.tools" />
-          <meta name="og:title" content="Solana NFT Toolbox" />
+          <meta name="og:title" content="Solana NFT Tools" />
           <meta name="og:image" content="https://arweave.net/jeC5bLjY2mY7mZJvnJ1aHTP1zh3oUSMGsalcrUwwJ28" />
           <meta
             property="og:description"
-            content="Solana NFT Tools, made by @0xAlice_"
+            content="Solana NFT Tools, made by @pentaclexyz"
           />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/brands.min.css" />
         </Head>
